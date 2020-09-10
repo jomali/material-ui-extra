2eebe3dc1cacd1ab56c0be9323211ff048665389
J. Francisco Martín <jfmartin@soincon.es>
2020-09-10 12:50:38 +0200

    feat: Add Box component

---
+++ src/examples/BoxExample.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Box from "components/Box";
+
+export default function BoxExample() {
+  return (
+    <Box margin={2} padding={2} spacing={2}>
+      <p>Lorem ipsum dolor.</p>
+      <p>Lorem ipsum dolor.</p>
+      <p>Lorem ipsum dolor.</p>
+      <p>Lorem ipsum dolor.</p>
+      <p>Lorem ipsum dolor.</p>
+    </Box>
+  );
+}
